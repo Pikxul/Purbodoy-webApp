6c41ffc662daf5f30443d97e1b2b131c956e1ad8
Pikxul <mrityunjoyop123@gmail.com>
2026-01-07 14:17:10 +0530

    enhancing ui/ux

--- app/page.tsx
+++ app/page.tsx
@@ -369,7 +369,7 @@ export default async function HomePage() {
                   <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"/>
                 </svg>
               </div>
-              <div className="metric-number">100,000+</div>
+              <div className="metric-number">10,000+</div>
               <div className="metric-label">Happy Travelers</div>
             </div>
             <div className="metric-item">
